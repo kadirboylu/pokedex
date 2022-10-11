11e89204e0ef20c45dddbbb7d5082ed897b7fe50
Kadir Boylu <kadir.boylu@outlook.com>
2022-10-11 17:51:16 +0300

    Update abilities, types and moves

--- src/mocks/pokemon.ts
+++ src/mocks/pokemon.ts
@@ -6,9 +6,41 @@ export const pikachu: PokemonResponse = {
   height: 4,
   weight: 60,
   base_experience: 112,
-  abilities: [],
-  types: [],
-  moves: [],
+  abilities: [
+    {
+      ability: {
+        name: "static",
+        url: "https://pokeapi.co/api/v2/ability/9/",
+      },
+      is_hidden: false,
+      slot: 1,
+    },
+    {
+      ability: {
+        name: "lightning-rod",
+        url: "https://pokeapi.co/api/v2/ability/31/",
+      },
+      is_hidden: true,
+      slot: 3,
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "electric",
+        url: "https://pokeapi.co/api/v2/type/13/",
+      },
+    },
+  ],
+  moves: [
+    {
+      move: {
+        name: "thunder-bolt",
+        url: "https://pokeapi.co/api/v2/move/85/",
+      },
+    },
+  ],
   sprites: {
     other: {
       home: {
@@ -24,9 +56,41 @@ export const charmander: PokemonResponse = {
   height: 6,
   weight: 85,
   base_experience: 62,
-  abilities: [],
-  types: [],
-  moves: [],
+  abilities: [
+    {
+      ability: {
+        name: "blaze",
+        url: "https://pokeapi.co/api/v2/ability/66/",
+      },
+      is_hidden: false,
+      slot: 1,
+    },
+    {
+      ability: {
+        name: "solar-power",
+        url: "https://pokeapi.co/api/v2/ability/94/",
+      },
+      is_hidden: true,
+      slot: 3,
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "fire",
+        url: "https://pokeapi.co/api/v2/type/10/",
+      },
+    },
+  ],
+  moves: [
+    {
+      move: {
+        name: "flame-burst",
+        url: "https://pokeapi.co/api/v2/move/394/",
+      },
+    },
+  ],
   sprites: {
     other: {
       home: {
