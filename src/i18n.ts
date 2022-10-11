014cabf420eb47fe2c740c7889b9b1e706126fd4
Kadir Boylu <kadir.boylu@outlook.com>
2022-10-11 14:39:16 +0300

    Modify i18n.ts and create en.ts and tr.ts files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,12 @@
 import Vue from "vue";
-import VueI18n, { LocaleMessages } from "vue-i18n";
+import VueI18n from "vue-i18n";
+import { en } from "./locales/en";
+import { tr } from "./locales/tr";
 
 Vue.use(VueI18n);
-
-function loadLocaleMessages(): LocaleMessages {
-  const locales = require.context("./locales", true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  const messages: LocaleMessages = {};
-  locales.keys().forEach((key) => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-    if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key);
-    }
-  });
-  return messages;
-}
 
 export default new VueI18n({
   locale: localStorage.getItem("language") || "en",
   fallbackLocale: localStorage.getItem("language") || "en",
-  messages: loadLocaleMessages(),
+  messages: { en, tr },
 });
